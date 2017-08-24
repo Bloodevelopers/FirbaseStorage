eccccfbc8e4fc5bd5b8adec3634ee41143677d4e
shubhamtunwal97 <shubhamtunwal97@gmail.com>
2017-08-25 01:35:30 +0530

    Added the intent to crossfade drawer,Problem: the onClick on GridView is taking long time to switch to other activity.Edit it in AllClassesAdapte .Solution Neededgit add .

--- app/src/main/java/com/example/aryansoni/cbse/AllClassesAdapter.java
+++ app/src/main/java/com/example/aryansoni/cbse/AllClassesAdapter.java
@@ -2,13 +2,13 @@ package com.example.aryansoni.cbse;
 
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.List;
 
@@ -32,28 +32,37 @@ public class AllClassesAdapter extends RecyclerView.Adapter<AllClassesAdapter.Re
 
     @Override
     public void onBindViewHolder(RecyclerViewHolders recyclerViewHolders, int i) {
-        int num=i+1;
-        recyclerViewHolders.countryName.setText("Class "+num);
+        int num = i + 1;
+        recyclerViewHolders.countryName.setText("Class " + num);
     }
 
     @Override
     public int getItemCount() {
         return this.itemList.size();
     }
-    public class RecyclerViewHolders extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+    public class RecyclerViewHolders extends RecyclerView.ViewHolder implements View.OnClickListener {
 
         TextView countryName;
         ImageView imgv;
 
         public RecyclerViewHolders(View itemView) {
             super(itemView);
-            countryName=itemView.findViewById(R.id.country_name);
-            imgv=itemView.findViewById(R.id.country_photo);
+            countryName = itemView.findViewById(R.id.country_name);
+            imgv = itemView.findViewById(R.id.country_photo);
             itemView.setOnClickListener(this);
         }
 
         @Override
         public void onClick(View view) {
-            Toast.makeText(view.getContext(), "Clicked Country Position = " + getPosition(), Toast.LENGTH_SHORT).show();
+
+            imgv.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent i = new Intent(context, CrossfadeDrawerLayoutActvitiy.class);
+                    context.startActivity(i);
+                }
+            });
         }
-}}
+    }
+}
