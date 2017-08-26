05a15f8f7c7a92bcad014ae32cd8b117a21054ff
Aryan Soni <aryansoni1108@gmail.com>
2017-08-26 19:04:05 +0530

    Test 2

--- app/src/main/java/com/example/aryansoni/cbse/AfterLogin.java
+++ app/src/main/java/com/example/aryansoni/cbse/AfterLogin.java
@@ -28,3 +28,12 @@ public class AfterLogin extends AppCompatActivity {
         });
     }
 }
+
+
+/*
+Concept of algorithm
+An algorithm is a set of rules for carrying out calculation that transform the input to the output.
+A common man’s belief is that a computer can do anything and everything that he imagines. It is very difficult to make people realize that it is not really the computer but the man behind computer who does everything.
+In the modern internet world man feels that just by entering what he wants to search into the computers he can get information as desired by him. He believes that, this is done by computer. A common man seldom understands that a man made procedure called search has done the entire job and the only support provided by the computer is the executional speed and organized storage of information.
+In the above instance, a designer of the information system should know what one frequently searches for. He should make a structured organization of all those details to store in memory of the computer. Based on the requirement, the right information is brought out. This is accomplished through a set of instructions created by the designer of the information system to search the right information matching the requirement of the user. This set of instructions is termed as program. It should be evident by now that it is not the computer, which generates automatically the program but it is the designer of the information system who has created this.
+ */
